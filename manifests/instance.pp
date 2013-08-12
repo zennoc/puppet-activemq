9c052a3f0752dd0ec1fa3bf25bee020bb6d5d5f1
Nathan Flynn <nathan.flynn@zeninternet.co.uk>
2013-08-12 22:17:50 +0100

    Example42'd the config for instances.

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,4 +1,4 @@
-define activemq::instance(
+define activemq::instance (
   $instance_name    = '',
   $openwire         = true,
   $openwire_port    = 6166,
@@ -6,20 +6,16 @@ define activemq::instance(
   $stomp_nio_port   = 6163,
   $stomp_queue      = true,
   $stomp_queue_port = 61613,
-
   $user_name        = "guest",
   $user_password    = "guest",
   $user_groups      = "users,everyone",
   $user_auth_queue  = '',
   $user_auth_topic  = '',
-
   $admin_name       = "admin",
   $admin_password   = "admin",
   $admin_groups     = "admins,everyone",
-  $admin_auth_queue  = ">",
-  $admin_auth_topic  = ">",
-) {
-
+  $admin_auth_queue = ">",
+  $admin_auth_topic = ">",) {
   if $instance_name != '' {
     $real_name = $instance_name
   } else {
@@ -38,44 +34,42 @@ define activemq::instance(
     $real_user_auth_topic = $user_auth_topic
   }
 
-
-  $instance_path = "/etc/activemq/instances-available/${real_name}"
-  $instance_enabled_path = "/etc/activemq/instances-enabled/${real_name}"
-
-  File {
-    notify => Class['activemq::service'],
-  }
+  $instance_path         = "${activemq::config_dir}/instances-available/${real_name}"
+  $instance_enabled_path = "${activemq::config_dir}/instances-enabled/${real_name}"
 
   file { $instance_path:
     ensure  => directory,
-    require => Class['activemq::config'],
+    require => Class['activemq::install'],
   }
 
-  file {"${instance_path}/activemq.xml":
+  file { "${instance_path}/activemq.xml":
     ensure  => present,
     content => template('activemq/activemq.xml.erb'),
     require => File[$instance_path],
+    notify  => $activemq::manage_service_autorestart,
   }
 
-  file {"${instance_path}/log4j.properties":
+  file { "${instance_path}/log4j.properties":
     ensure  => present,
     content => template('activemq/log4j.properties.erb'),
     require => File[$instance_path],
+    notify  => $activemq::manage_service_autorestart,
   }
 
-  file {"${instance_path}/credentials.properties":
+  file { "${instance_path}/credentials.properties":
     ensure  => present,
     content => template('activemq/credentials.properties.erb'),
     require => File[$instance_path],
+    notify  => $activemq::manage_service_autorestart,
   }
 
   file { $instance_enabled_path:
-    ensure => link,
-    target => $instance_path,
+    ensure  => link,
+    target  => $instance_path,
     require => [
       File["${instance_path}/activemq.xml"],
       File["${instance_path}/log4j.properties"],
       File["${instance_path}/credentials.properties"],
-    ],
+      ],
   }
 }
